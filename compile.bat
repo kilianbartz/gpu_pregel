81c509d70a9890c83d69ae7a733f2a04cdb8c7f8
Kilian Bartz <s4kibart@uni-trier.de>
2024-09-05 14:18:24 +0200

    erster Erfolg: funktionierendes PageRank für 2 Nodes

--- compile.bat
+++ compile.bat
@@ -1,1 +1,7 @@
-hipcc .\main.hip -I "C:\ROCm\include" -L "C:\ROCm\lib" --offload-arch=gfx1030
+IF "%~1"=="" (
+    SET INPUT_FILE=.\main.hip
+) ELSE (
+    SET INPUT_FILE=%~1
+)
+
+hipcc %INPUT_FILE% -I "C:\ROCm\include" -L "C:\ROCm\lib" --offload-arch=gfx1030
